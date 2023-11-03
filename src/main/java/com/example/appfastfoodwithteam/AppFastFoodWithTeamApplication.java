86e85d491f3e7f4b7d3dc1f723fbca324e1dd6e5
sehrli <sehrlia02@gmail.com>
2023-11-03 21:35:44 +0500

    initial

---
+++ src/main/java/com/example/appfastfoodwithteam/AppFastFoodWithTeamApplication.java
@@ -0,0 +1,13 @@
+package com.example.appfastfoodwithteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFastFoodWithTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppFastFoodWithTeamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appfastfoodwithteam/AppFastFoodWithTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appfastfoodwithteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFastFoodWithTeamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
